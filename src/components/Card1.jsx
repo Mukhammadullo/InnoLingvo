e35526109796dd812471b8c72b4ddbab9c83d23d
Muhammadullo <mnastulloev@gmail.com>
2023-12-24 15:16:58 +0500

    fix

--- src/components/Card1.jsx
+++ src/components/Card1.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Card1 = ({ img, title, txt,innertxt }) => {
   return (
-    <div className='md:w-[40%] md:h-[40vh] shadow-2xl mob:m-[20px] md:m-0 p-[3%]'>
+    <div className='md:w-[40%] md:h-[40vh] shadow-2xl dark:shadow-[white] rounded-[12px] mob:m-[20px] md:m-0 p-[3%]'>
       <img src={img} />
       <h1 className='font-bold text-[20px] m-[10px] dark:text-[white]'>{title}</h1>
       <p className='text-[#535353] m-[10px] dark:text-[white]'>{txt}</p>
